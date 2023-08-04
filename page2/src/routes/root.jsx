0cb4f8b0d06850943085d36f1528850d6c7d4dd6
Jack Sherick <sherij2@rpi.edu>
2023-08-04 14:13:29 -0400

    modified:   page2/src/routes/root.jsx

--- page2/src/routes/root.jsx
+++ page2/src/routes/root.jsx
@@ -8,7 +8,6 @@ export default function Root() {
       </head>
 
       <body>
-        
       <Helmet>
         <script src='https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.14.2/matter.min.js'></script>
         <script src="/src/routes/background.js"></script>
@@ -35,7 +34,7 @@ export default function Root() {
       </nav>
 
       <h1>About Me</h1>
-      <p>Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.</p>
+      <p>Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself...  </p>
 
       </div>
       </body>
